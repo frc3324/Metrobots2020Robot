58aeb2ce3b7feb5b2a2ad62d2d2dc07cb2b014b4
jmassy <55810776+jmassy@users.noreply.github.com>
2020-01-13 16:16:06 -0500

    Update Intake.kt
    made work

--- src/main/kotlin/frc/team3324/robot/intake/Intake.kt
+++ src/main/kotlin/frc/team3324/robot/intake/Intake.kt
@@ -1,14 +1,17 @@
 package frc.team3324.robot.intake
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 
 class Intake : SubsystemBase() {
     private val leftMotor = WPI_TalonSRX(0)
     private val rightMotor = WPI_TalonSRX(1)
+    private var i = 0
+    private var motorCurrent: Double = 0.0
 
     init {
-        var i = 0
         leftMotor.configContinuousCurrentLimit(20)
         leftMotor.enableCurrentLimit(true)
         rightMotor.follow(leftMotor)
@@ -18,8 +21,8 @@ class Intake : SubsystemBase() {
 
     fun run(power: Double) {
         leftMotor.set(power)
-        var motorcurrent = getCurrent(leftMotor)
-        putData(motorcurrent, i)
+        motorCurrent = leftMotor.statorCurrent
+        SmartDashboard.putData(motorCurrent.toString(), SmartDashboard.getData(i.toString()))
         i++
     }
 
