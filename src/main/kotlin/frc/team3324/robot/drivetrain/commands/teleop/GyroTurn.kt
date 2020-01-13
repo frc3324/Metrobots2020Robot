aa0796d16d0b5a1e41c3bc21e0ea71cf7277dbad
TotallyNotDaniel <burke.1@themetroschool.org>
2020-01-13 16:41:18 -0500

    Added code for the gyro to make it work.

---
+++ src/main/kotlin/frc/team3324/robot/drivetrain/commands/teleop/GyroTurn.kt
@@ -0,0 +1,27 @@
+package frc.team3324.robot.drivetrain.commands.teleop
+
+
+import com.kauailabs.navx.frc.AHRS
+import edu.wpi.first.networktables.NetworkTableEntry
+import edu.wpi.first.wpilibj.SPI
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+
+
+private val gyro = AHRS(SPI.Port.kMXP)
+private val sensorTab = Shuffleboard.getTab("Encoder Values")
+private val gyroYaw: NetworkTableEntry = sensorTab.add("Gyro Yaw", 0).withPosition(0, 2).getEntry()
+
+
+fun clearGyro() {
+    gyro.reset()
+}
+
+object GetYaw{
+    fun getYaw(): Double {
+        return gyro.yaw.toDouble()
+    }
+}
+
+fun updateSensors() {
+    gyroYaw.setNumber(0)
+}
