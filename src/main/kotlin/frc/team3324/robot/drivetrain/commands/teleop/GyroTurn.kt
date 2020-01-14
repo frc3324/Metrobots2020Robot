7314f7bb6ed88ecc0b0c207ddd0870650031db51
TotallyNotDaniel <burke.1@themetroschool.org>
2020-01-14 15:16:01 -0500

    Added class to this because i deleted it lol

--- src/main/kotlin/frc/team3324/robot/drivetrain/commands/teleop/GyroTurn.kt
+++ src/main/kotlin/frc/team3324/robot/drivetrain/commands/teleop/GyroTurn.kt
@@ -11,17 +11,18 @@ private val gyro = AHRS(SPI.Port.kMXP)
 private val sensorTab = Shuffleboard.getTab("Encoder Values")
 private val gyroYaw: NetworkTableEntry = sensorTab.add("Gyro Yaw", 0).withPosition(0, 2).getEntry()
 
-
-fun clearGyro() {
-    gyro.reset()
-}
-
-object GetYaw{
-    fun getYaw(): Double {
-        return gyro.yaw.toDouble()
+class GyroTurn {
+    fun clearGyro() {
+        gyro.reset()
     }
-}
 
-fun updateSensors() {
-    gyroYaw.setNumber(0)
+    object GetYaw {
+        fun getYaw(): Double {
+            return gyro.yaw.toDouble()
+        }
+    }
+
+    fun updateSensors() {
+        gyroYaw.setNumber(0)
+    }
 }
