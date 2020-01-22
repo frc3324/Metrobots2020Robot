d6d2cd49799f1a20b3be05af3839d3c43b908da3
Team Laptop <team3324metrobots@gmail.com>
2020-01-22 18:28:19 -0500

    Add switch relay

--- src/main/kotlin/frc/team3324/robot/Robot.kt
+++ src/main/kotlin/frc/team3324/robot/Robot.kt
@@ -2,6 +2,7 @@ package frc.team3324.robot
 
 import edu.wpi.first.wpilibj.Compressor
 import edu.wpi.first.wpilibj.PowerDistributionPanel
+import edu.wpi.first.wpilibj.Relay
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj.livewindow.LiveWindow
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
