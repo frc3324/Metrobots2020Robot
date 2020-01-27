b70df4c2cf176905285d7996eae65abac9808c90
Erin Ford <audricford@gmail.com>
2020-01-27 16:35:21 -0500

    Added oblog to gradle build and robotinit/robotperiodic, also created logger file to put getters and setters in Public function to assign a value and tab in Shuffleboard

--- src/main/kotlin/frc/team3324/robot/util/Moggers.kt
+++ src/main/kotlin/frc/team3324/robot/util/Moggers.kt
@@ -2,7 +2,14 @@ package frc.team3324.robot.util
 
 import io.github.oblarg.oblog.annotations.Log
 import io.github.oblarg.oblog.annotations.Config
+import edu.wpi.first.wpilibj.livewindow.LiveWindow
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
+
 class Moggers {
-
-
+    public fun addToLog(data:Double, tab:String, valueTitle:String){
+        Shuffleboard.getTab(tab)
+                .add(valueTitle,data)
+    }
 }
