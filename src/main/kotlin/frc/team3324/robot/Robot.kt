1aa643b9c6fd13171fd37d9fa87ce9fe6ebc1718
MoormanA <moorman.1@themetroschool.org>
2020-01-28 17:10:06 -0500

    Moggers
    RIP SmartDashboard, 2014-2020
    
    "Nobody loves you, SmartDashboard"
    --Carter

--- src/main/kotlin/frc/team3324/robot/Robot.kt
+++ src/main/kotlin/frc/team3324/robot/Robot.kt
@@ -8,7 +8,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import edu.wpi.first.wpilibj.AnalogInput
 import com.cuforge.libcu.Lasershark
-import io.github.oblarg.oblog.Logger
+import frc.team3324.robot.util.Moggers
+
+//import io.github.oblarg.oblog.Logger
 
 class Robot: TimedRobot() {
     private val compressor = Compressor()
@@ -27,7 +29,7 @@ class Robot: TimedRobot() {
     override fun robotInit() {
         LiveWindow.disableAllTelemetry()
         compressor.start()
-        Logger.configureLoggingAndConfig(true,false)
+        //Logger.configureLoggingAndConfig(true,false)
     }
 
     fun enabledInit() {
@@ -35,9 +37,11 @@ class Robot: TimedRobot() {
 
     override fun robotPeriodic() {
         CommandScheduler.getInstance().run()
-        SmartDashboard.putNumber("Ultrasonic Inch Distance: ", depRangeInches)
-        SmartDashboard.putNumber("Lidar Meter Distance: ", rangeMeters)
-        Logger.updateEntries()
+        /*SmartDashboard.putNumber("Ultrasonic Inch Distance: ", depRangeInches)
+        SmartDashboard.putNumber("Lidar Meter Distance: ", rangeMeters)*/
+        Moggers.addToLog(depRangeInches, "Robot", "Ultrasonic Inch Distance: ")
+        Moggers.addToLog(rangeMeters, "Robot", "Lidar Meter Distance: ")
+        //Logger.updateEntries()
     }
 
     override fun autonomousInit() {
--- src/main/kotlin/frc/team3324/robot/util/Moggers.kt
+++ src/main/kotlin/frc/team3324/robot/util/Moggers.kt
@@ -1,15 +1,18 @@
 package frc.team3324.robot.util
 
-import io.github.oblarg.oblog.annotations.Log
-import io.github.oblarg.oblog.annotations.Config
+//import io.github.oblarg.oblog.annotations.Log
+//import io.github.oblarg.oblog.annotations.Config
 import edu.wpi.first.wpilibj.livewindow.LiveWindow
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 
-class Moggers {
+public object Moggers {
     public fun addToLog(data:Double, tab:String, valueTitle:String){
         Shuffleboard.getTab(tab)
                 .add(valueTitle,data)
+    }
+    public fun getValue(tab:String, valueTitle: String): Double {
+        return Shuffleboard.getTab(tab).add(valueTitle, 0.0).entry.getDouble(0.0)
     }
 }
