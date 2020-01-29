4957ea8479267e5ac90fcc5466081d4d92c2e17b
Team Laptop <team3324metrobots@gmail.com>
2020-01-29 16:43:49 -0500

    Logger
    Added Oblog, Updated Moggers to use Oblog

--- src/main/kotlin/frc/team3324/robot/util/Moggers.kt
+++ src/main/kotlin/frc/team3324/robot/util/Moggers.kt
@@ -1,7 +1,7 @@
 package frc.team3324.robot.util
 
-//import io.github.oblarg.oblog.annotations.Log
-//import io.github.oblarg.oblog.annotations.Config
+import io.github.oblarg.oblog.annotations.Log
+import io.github.oblarg.oblog.annotations.Config
 import edu.wpi.first.wpilibj.livewindow.LiveWindow
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
@@ -9,13 +9,16 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 
 public object Moggers {
+    @Config
     public fun addToLog(data:Double, tab:String, valueTitle:String){
         Shuffleboard.getTab(tab)
                 .add(valueTitle,data)
     }
+    @Config
     public fun getValue(tab:String, valueTitle: String): Double {
         return Shuffleboard.getTab(tab).add(valueTitle, 0.0).entry.getDouble(0.0)
     }
+    @Config
     public fun addChooser(chooser: SendableChooser<Double>, tab: String, valueTitle: String){
         Shuffleboard.getTab(tab)
                 .add(valueTitle, chooser)
