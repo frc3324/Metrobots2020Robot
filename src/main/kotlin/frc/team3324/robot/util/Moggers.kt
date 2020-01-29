35fe2d139f88517f54ad885d75eacea207de0953
Team Laptop <team3324metrobots@gmail.com>
2020-01-29 16:44:58 -0500

    Master Logger
    Added Oblog to Moggers in Master Branch

--- src/main/kotlin/frc/team3324/robot/util/Moggers.kt
+++ src/main/kotlin/frc/team3324/robot/util/Moggers.kt
@@ -1,17 +1,19 @@
 package frc.team3324.robot.util
 
-//import io.github.oblarg.oblog.annotations.Log
-//import io.github.oblarg.oblog.annotations.Config
+import io.github.oblarg.oblog.annotations.Log
+import io.github.oblarg.oblog.annotations.Config
 import edu.wpi.first.wpilibj.livewindow.LiveWindow
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 
 public object Moggers {
+    @Config
     public fun addToLog(data:Double, tab:String, valueTitle:String){
         Shuffleboard.getTab(tab)
                 .add(valueTitle,data)
     }
+    @Config
     public fun getValue(tab:String, valueTitle: String): Double {
         return Shuffleboard.getTab(tab).add(valueTitle, 0.0).entry.getDouble(0.0)
     }
