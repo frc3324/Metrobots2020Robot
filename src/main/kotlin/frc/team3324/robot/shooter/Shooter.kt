0075f3da0930c0fc2b9ee75c86bc8d3e3b1bd472
Team Laptop <team3324metrobots@gmail.com>
2020-01-29 16:13:27 -0500

    Shuffleboard

--- src/main/kotlin/frc/team3324/robot/shooter/Shooter.kt
+++ src/main/kotlin/frc/team3324/robot/shooter/Shooter.kt
@@ -13,98 +13,96 @@ import frc.team3324.robot.util.physics.Motors.getVoltageFromRPM
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 class Shooter: SubsystemBase(), Loggable {
     val leftMotor = CANSparkMax(Consts.Shooter.LEFT_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless)
-    val rightMotor = CANSparkMax(Consts.Shooter.RIGHT_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless)
-    val leftEncoder = leftMotor.encoder
-    val rightEncoder = rightMotor.encoder
+        val rightMotor = CANSparkMax(Consts.Shooter.RIGHT_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless)
+        val leftEncoder = leftMotor.encoder
+        val rightEncoder = rightMotor.encoder
 
-    val motor = Motors.Neo(2)
-    val rpmChooser = SendableChooser<Double>()
+        val motor = Motors.Neo(2)
+        val rpmChooser = SendableChooser<Double>()
 
-    val rightEncoderPosition
-        @Log
-        get() = rightEncoder.position
-    val rightEncoderVelocity
-        @Log
-        get() = rightEncoder.velocity
+        val rightEncoderPosition
+            @Log
+            get() = rightEncoder.position
+        val rightEncoderVelocity
+            @Log
+            get() = rightEncoder.velocity
 
-    var rightMotorSpeed
-        @Log
-        get() = rightMotor.get()
-        set(rightMotorSpeed) = rightMotor.set(rightMotorSpeed)
-    val rightMotorCurrent
-        @Log
-        get() = rightMotor.outputCurrent
+        var rightMotorSpeed
+            @Log
+            get() = rightMotor.get()
+            set(rightMotorSpeed) = rightMotor.set(rightMotorSpeed)
+        val rightMotorCurrent
+            @Log
+            get() = rightMotor.outputCurrent
 
-    val leftEncoderPosition
-        @Log
-        get() = leftEncoder.position
-    val leftEncoderVelocity
-        @Log
-        get() = leftEncoder.velocity
+        val leftEncoderPosition
+            @Log
+            get() = leftEncoder.position
+        val leftEncoderVelocity
+            @Log
+            get() = leftEncoder.velocity
 
-    var leftMotorSpeed
-        @Log
-        get() = leftMotor.get()
-        set(leftMotorSpeed) = leftMotor.set(leftMotorSpeed)
-    val leftMotorCurrent
-        @Log
-        get() = leftMotor.outputCurrent
+        var leftMotorSpeed
+            @Log
+            get() = leftMotor.get()
+            set(leftMotorSpeed) = leftMotor.set(leftMotorSpeed)
+        val leftMotorCurrent
+            @Log
+            get() = leftMotor.outputCurrent
 
-    var RPM: Double
-        @Log
-        get() = leftEncoder.velocity
-        set(rpm) = leftMotor.setVoltage(runPID(rpm))
+        var RPM: Double
+            @Log
+            get() = leftEncoder.velocity
+            set(rpm) = leftMotor.setVoltage(runPID(rpm))
 
-    var percentPower: Double
-        get() = leftMotor.get()
-        set(speed) = leftMotor.set(speed)
+        var percentPower: Double
+            get() = leftMotor.get()
+            set(speed) = leftMotor.set(speed)
 
-    init {
-        rightMotor.restoreFactoryDefaults()
-        leftMotor.restoreFactoryDefaults()
-        rightMotor.idleMode = CANSparkMax.IdleMode.kCoast
-        leftMotor.idleMode = CANSparkMax.IdleMode.kCoast
-        rightMotor.follow(leftMotor, true)
+        init {
+            rightMotor.restoreFactoryDefaults()
+            leftMotor.restoreFactoryDefaults()
+            rightMotor.idleMode = CANSparkMax.IdleMode.kCoast
+            leftMotor.idleMode = CANSparkMax.IdleMode.kCoast
+            rightMotor.follow(leftMotor, true)
 
-        leftMotor.inverted = false
-        leftEncoder.velocityConversionFactor = Consts.Shooter.GEAR_RATIO
-        rightEncoder.velocityConversionFactor = Consts.Shooter.GEAR_RATIO
-        leftMotor.setSmartCurrentLimit(40)
-        rightMotor.setSmartCurrentLimit(40)
+            leftMotor.inverted = false
+            leftEncoder.velocityConversionFactor = Consts.Shooter.GEAR_RATIO
+            rightEncoder.velocityConversionFactor = Consts.Shooter.GEAR_RATIO
+            leftMotor.setSmartCurrentLimit(40)
+            rightMotor.setSmartCurrentLimit(40)
 
-        rpmChooser.addOption("0", 0.0)
-        rpmChooser.addOption("200", 200.0)
-        rpmChooser.addOption("1000", 1000.0)
-        rpmChooser.addOption("1500", 1500.0)
-        rpmChooser.addOption("2000", 2000.0)
-        rpmChooser.addOption("2500", 2500.0)
-        rpmChooser.addOption("3000", 3000.0)
-        rpmChooser.addOption("3500", 3500.0)
-        rpmChooser.addOption("4000", 4000.0)
-        rpmChooser.addOption("4500", 4500.0)
-        rpmChooser.addOption("4750", 4750.0)
-        rpmChooser.addOption("4800", 4800.0)
-        rpmChooser.addOption("4875", 4875.0)
-        rpmChooser.addOption("5250", 5250.0)
-        rpmChooser.addOption("5500", 5500.0)
-        rpmChooser.addOption("6000", 6000.0)
-        rpmChooser.addOption("6500", 6500.0)
-        rpmChooser.setDefaultOption("0", 0.0)
-        SmartDashboard.putData("RPM Chooser:", rpmChooser)
-    }
+            rpmChooser.addOption("0", 0.0)
+            rpmChooser.addOption("200", 200.0)
+            rpmChooser.addOption("1000", 1000.0)
+            rpmChooser.addOption("1500", 1500.0)
+            rpmChooser.addOption("2000", 2000.0)
+            rpmChooser.addOption("2500", 2500.0)
+            rpmChooser.addOption("3000", 3000.0)
+            rpmChooser.addOption("3500", 3500.0)
+            rpmChooser.addOption("4000", 4000.0)
+            rpmChooser.addOption("4500", 4500.0)
+            rpmChooser.addOption("4750", 4750.0)
+            rpmChooser.addOption("4800", 4800.0)
+            rpmChooser.addOption("4875", 4875.0)
+            rpmChooser.addOption("5250", 5250.0)
+            rpmChooser.addOption("5500", 5500.0)
+            rpmChooser.addOption("6000", 6000.0)
+            rpmChooser.addOption("6500", 6500.0)
+            rpmChooser.setDefaultOption("0", 0.0)
+        }
 
-    fun runPID(desiredSpeed: Double): Double {
-        val error = desiredSpeed - RPM
-        val pValue = Consts.Shooter.Kp * error
-        return pValue + (desiredSpeed * Consts.Shooter.Kv) + Consts.Shooter.Ks
-    }
+        fun runPID(desiredSpeed: Double): Double {
+            val error = desiredSpeed - RPM
+            val pValue = Consts.Shooter.Kp * error
+            return pValue + (desiredSpeed * Consts.Shooter.Kv) + Consts.Shooter.Ks
+        }
 
-    override fun periodic() {
-        SmartDashboard.putNumber("RPM ", RPM)
-        SmartDashboard.putNumber("Amp", leftMotor.outputCurrent)
-        if (rpmChooser.selected != null) {
-            RPM = rpmChooser.selected
+        override fun periodic() {
+            SmartDashboard.putNumber("RPM ", RPM)
+            SmartDashboard.putNumber("Amp", leftMotor.outputCurrent)
+            if (rpmChooser.selected != null) {
+                RPM = rpmChooser.selected
+            }
         }
     }
-
-}
