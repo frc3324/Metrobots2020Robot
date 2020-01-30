e92dab4935fb5619f611d6dfa5c06ecc1e0ebd5c
jmassy <55810776+jmassy@users.noreply.github.com>
2020-01-30 16:45:42 -0500

    Added Logger in Intake and Shooter

--- src/main/kotlin/frc/team3324/robot/intake/Intake.kt
+++ src/main/kotlin/frc/team3324/robot/intake/Intake.kt
@@ -4,19 +4,29 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj.DutyCycleEncoder
+import io.github.oblarg.oblog.annotations.Log
 
 class Intake : SubsystemBase() {
     private val leftMotor = WPI_TalonSRX(0)
     private val rightMotor = WPI_TalonSRX(1)
     private val dutyEncoder = DutyCycleEncoder(7)
+    var leftMotorSpeed
+        @Log
+        get() = leftMotor.get()
+        set(LeftMotorSpeed) = leftMotor.set(leftMotorSpeed)
+
+    val leftMotorCurrent
+        @Log
+        get() = leftMotor.statorCurrent
+
     private val radianMeasure: Double
+        @Log
         get() = dutyEncoder.get()*2*Math.PI
 
     init {
         leftMotor.configContinuousCurrentLimit(20)
         leftMotor.enableCurrentLimit(true)
         rightMotor.follow(leftMotor)
-
     }
 
 
--- src/main/kotlin/frc/team3324/robot/shooter/Shooter.kt
+++ src/main/kotlin/frc/team3324/robot/shooter/Shooter.kt
@@ -6,15 +6,48 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.team3324.robot.util.Consts
 import frc.team3324.robot.util.physics.Motors
 import frc.team3324.robot.util.physics.Motors.getPercentFromRPM
+import io.github.oblarg.oblog.annotations.Log
 
 class Shooter: SubsystemBase() {
     val leftMotor = CANSparkMax(Consts.Shooter.LEFT_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless)
     val rightMotor = CANSparkMax(Consts.Shooter.RIGHT_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless)
     val leftEncoder = leftMotor.encoder
     val rightEncoder = rightMotor.encoder
+
     val motor = Motors.Neo(2)
 
+    val rightEncoderPosition
+        @Log
+        get() = rightEncoder.position
+    val rightEncoderVelocity
+        @Log
+        get() = rightEncoder.velocity
+
+    var rightMotorSpeed
+        @Log
+        get() = rightMotor.get()
+        set(rightMotorSpeed) = rightMotor.set(rightMotorSpeed)
+    val rightMotorCurrent
+        @Log
+        get() = rightMotor.outputCurrent
+
+    val leftEncoderPosition
+        @Log
+        get() = leftEncoder.position
+    val leftEncoderVelocity
+        @Log
+        get() = leftEncoder.velocity
+
+    var leftMotorSpeed
+        @Log
+        get() = leftMotor.get()
+        set(leftMotorSpeed) = leftMotor.set(leftMotorSpeed)
+    val leftMotorCurrent
+        @Log
+        get() = leftMotor.outputCurrent
+
     var RPM: Double
+        @Log
         get() = leftEncoder.velocity
         set(rpm) = leftMotor.setVoltage(runPID(rpm))
 
