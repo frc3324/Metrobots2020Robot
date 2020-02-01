77dd96491fa5eec0277d2c2275d9a37edefdf8f2
Mani Kamali <kamalimani1623@gmail.com>
2020-02-01 14:40:54 -0500

    Added ramsete controller

--- src/main/kotlin/frc/team3324/robot/RobotContainer.kt
+++ src/main/kotlin/frc/team3324/robot/RobotContainer.kt
@@ -4,7 +4,18 @@ import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.Relay
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.XboxController.Button
+import edu.wpi.first.wpilibj.controller.RamseteController
+import edu.wpi.first.wpilibj.geometry.Pose2d
+import edu.wpi.first.wpilibj.geometry.Rotation2d
+import edu.wpi.first.wpilibj.geometry.Translation2d
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint
+import edu.wpi.first.wpilibj.util.Units
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.CommandBase
+import edu.wpi.first.wpilibj2.command.RamseteCommand
 import edu.wpi.first.wpilibj2.command.button.JoystickButton
 import frc.team3324.robot.drivetrain.DriveTrain
 import frc.team3324.robot.drivetrain.commands.teleop.Drive
@@ -20,6 +31,8 @@ import frc.team3324.robot.util.Consts
 import frc.team3324.robot.util.PneumaticShift
 import frc.team3324.robot.util.SwitchRelay
 import io.github.oblarg.oblog.Logger
+import java.util.function.BiConsumer
+import java.util.function.Supplier
 
 class RobotContainer {
     private val intake = Intake()
@@ -59,5 +72,35 @@ class RobotContainer {
         JoystickButton(secondaryController, Button.kBumperRight.value).whileHeld(RunPivot(intake, -0.2))
         JoystickButton(secondaryController, Button.kA.value).whileHeld(RunIntake(intake, -1.0))
 //        JoystickButton(secondaryController, Button.kA.value).whenPressed(RunShooter(shooter, 5000.0))
+    }
+
+    fun getAutoCommand(): Command {
+        val config = TrajectoryConfig(Consts.DriveTrain.LOW_GEAR_MAX_VELOCITY, Consts.DriveTrain.LOW_GEAR_MAX_ACCELERATION)
+        config.setKinematics(driveTrain.driveKinematics)
+
+        val start = Pose2d(Units.feetToMeters(0.0), Units.feetToMeters(0.0), Rotation2d(0.0))
+        val end = Pose2d(Units.feetToMeters(1.0), Units.feetToMeters(0.0), Rotation2d(0.0))
+
+        val interiorWaypoints = listOf(Translation2d(0.5, 0.0))
+        val voltageConstraint = DifferentialDriveVoltageConstraint(driveTrain.feedForward, driveTrain.driveKinematics, 7.0)
+
+        val trajectory = TrajectoryGenerator.generateTrajectory(
+                start,
+                interiorWaypoints,
+                end,
+                config)
+
+        val ramseteCommand = RamseteCommand(
+                trajectory,
+                Supplier {driveTrain.pose},
+                RamseteController(Consts.DriveTrain.kRamseteB, Consts.DriveTrain.kRamseteZeta),
+                driveTrain.feedForward,
+                driveTrain.driveKinematics,
+                Supplier {driveTrain.wheelSpeeds},
+                driveTrain.leftPIDController,
+                driveTrain.rightPIDController,
+                BiConsumer(driveTrain::tankDriveVolts),
+                driveTrain)
+        return ramseteCommand
     }
 }
