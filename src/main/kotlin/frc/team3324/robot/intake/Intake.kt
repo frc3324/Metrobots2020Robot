effe5b1b6711c73a3c3dbee486e6ea1238877446
Andrew <kandrew2022@gmail.com>
2020-07-24 19:01:04 -0400

    Add Intake and Pivot Motors to Consts
    Added the motor for intake and the motor for pivot to Consts.kt so as to
    put all of the motor declarations in the same place. Also changed
    Intake.kt to use the motor from consts instead of declaring its own.

--- src/main/kotlin/frc/team3324/robot/intake/Intake.kt
+++ src/main/kotlin/frc/team3324/robot/intake/Intake.kt
@@ -4,10 +4,11 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import edu.wpi.first.wpilibj.DutyCycleEncoder
 import frc.team3324.library.motorcontrollers.MetroTalonSRX
 import frc.team3324.library.subsystems.MotorSubsystem
+import frc.team3324.robot.util.Consts
 import io.github.oblarg.oblog.Loggable
 import io.github.oblarg.oblog.annotations.Log
 
-class Intake: MotorSubsystem(listOf(MetroTalonSRX(26, 20)), 10.0), Loggable {
+class Intake: MotorSubsystem(listOf(Consts.Intake.MOTOR)), Loggable {
     private val dutyEncoder = DutyCycleEncoder(7)
 
     init {
